df4f62ca3ca72492f0f4ce34be500630281b799e
Tanvir Ishrak <tanvirishrak04@gmail.com>
2026-02-16 16:48:43 +0600

    switch to HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'   // <-- change here
-import { LanguageProvider } from './contexts/LanguageContext'
-import { ThemeProvider } from './contexts/ThemeContext'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // <-- use HashRouter
+import { LanguageProvider } from "./contexts/LanguageContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>   {/* <-- use HashRouter */}
-      <ThemeProvider>
-        <LanguageProvider>
-          <App />
-        </LanguageProvider>
-      </ThemeProvider>
-    </HashRouter>
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<HashRouter>
+			<ThemeProvider>
+				<LanguageProvider>
+					<App />
+				</LanguageProvider>
+			</ThemeProvider>
+		</HashRouter>
+	</React.StrictMode>,
+);
