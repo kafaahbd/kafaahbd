a9ed4c2091f1933b0142680d62b3262d12bc9c32
Tanvir Ishrak <tanvirishrak04@gmail.com>
2026-02-16 17:40:12 +0600

    jhamella

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kafaahbd/', // Replace with your repository name
+  base: '/kafaahbd/', // এই লাইনটি আগে ছিল, রাখুন
 })
